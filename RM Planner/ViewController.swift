f1bb47d77f41aff7f01316212a3696055fbd9248
Micah Wilson <MicahW@Micahs-MacBook-Pro.local>
2015-08-28 17:53:00 -0600

    Initial Commit

---
+++ RM Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RM Planner
+//
+//  Created by Micah Wilson on 8/28/15.
+//  Copyright (c) 2015 Micah Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
